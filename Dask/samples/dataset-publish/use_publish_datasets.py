7f15967ce25ee0949a1648cb6fbff9c1fc8093c0
wujinzhong <52945455+wujinzhong@users.noreply.github.com>
2019-11-15 14:08:24 +0800

    Update use_publish_datasets.py

--- Dask/samples/dataset-publish/use_publish_datasets.py
+++ Dask/samples/dataset-publish/use_publish_datasets.py
@@ -5,7 +5,7 @@ import cudf
 import dask_cudf
 from dask.distributed import Client
 
-client = Client("192.168.99.3:8487")
+client = Client("127.0.0.1:8487")
 print(client.list_datasets())
 shared_ddf = client.get_dataset('shared_dataset1')
 print(shared_ddf)
